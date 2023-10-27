f576bc7b75c8bf361ce7cb57d998c38218aeb7f7
Automação Elgin/Bematech <78883867+ElginDeveloperCommunity@users.noreply.github.com>
2023-10-27 13:49:48 -0300

    Atualizando estrutura

--- E1_Bridge/Protocolo_Aberto_Elgin_Bridge/server-exemplo-comunicacao/server-typescript/dist/logger.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function turnOnDebugger(): void;
-export declare const logger: {
-    debug(...message: string[]): void;
-    info(...message: string[]): void;
-};
-//# sourceMappingURL=logger.d.ts.map
--- E1_Bridge/Protocolo_Aberto_Elgin_Bridge/server-exemplo-comunicacao/server-typescript/dist/statusServer.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import net from 'node:net';
-export declare class StatusServer {
-    private server;
-    constructor();
-    handleConnection(socket: net.Socket): void;
-    listen(port: number, host: string, callback: () => void): void;
-}
-//# sourceMappingURL=statusServer.d.ts.map
